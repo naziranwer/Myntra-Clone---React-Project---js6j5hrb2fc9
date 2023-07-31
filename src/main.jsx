6f553e421386b2792f4a7a72946a5e7331657d53
Mohd Nazir Anwer <anwernazir674@gmail.com>
2023-07-31 13:47:41 +0530

    initial

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+// import { Router } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
