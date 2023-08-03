40130f4e19508f2bc5fba4c60144e46f219e8022
Mohd Nazir Anwer <anwernazir674@gmail.com>
2023-08-04 00:18:40 +0530

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,42 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// import App from "./App";
+// import "./index.css";
+// import store from "./redux/store";
+// import { Provider } from "react-redux";
+// // import { Router } from 'react-router-dom'
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </Provider>
+// );
+
 import React from "react";
-import ReactDOM from "react-dom/client";
+
+import ReactDOM from "react-dom";
 
 import App from "./App";
+
 import "./index.css";
+
 import store from "./redux/store";
+
 import { Provider } from "react-redux";
+
 // import { Router } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>
+  </Provider>,
+
+  document.getElementById("root")
 );
+
+reportWebVitals();
